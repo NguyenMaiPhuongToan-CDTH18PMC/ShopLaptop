803c852d2fda049890a6a4d14bdffc7695555b01
bicasmen <0306181281@caothang.edu.vn>
2021-05-14 13:01:46 +0700

    Giao Dien Admin co ban

---
+++ ShopLapTop/ShopLapTop.Shared/Models/LocalUserInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopLapTop.Shared.Models
+{
+    class LocalUserInfo
+    {
+        public string Email { get; set; }
+    }
+}
