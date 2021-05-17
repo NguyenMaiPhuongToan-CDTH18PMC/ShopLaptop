b3c9a52539886a49b06fc7e6d6111aa0ad4dd18c
HoangPham718 <46109091+HoangPham718@users.noreply.github.com>
2021-05-17 18:05:53 +0700

    Nhúng map

--- ShopLapTop/ShopLaptop.Client/Controllers/HomeController.cs
+++ ShopLapTop/ShopLaptop.Client/Controllers/HomeController.cs
@@ -47,6 +47,10 @@ namespace Client.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
